13d6fa8d77a0e1bc82d0de2d34141b946736afa1
Daniel Tran <tran.daniel95@gmail.com>
2017-03-07 17:06:08 -0500

    Adding databse to server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var app = express();
+// modules==================================================================
+var express     = require('express');
+var app         = express();
 
+// config files ============================================================
+var db = require('./config/db') ;
+
+// port=====================================================================
 var port = process.env.PORT || 8080;
-//Set port:8080
+
+// db connection============================================================
+// mongoose.connect(db.url) ;
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/layouts/page.html');
