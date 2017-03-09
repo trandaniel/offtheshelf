478e1e8155c06b96790ff2b14ad4443e7710935d
Daniel Tran <tran.daniel95@gmail.com>
2017-03-09 09:15:46 -0500

    Updating profile object model

--- app/models/profile.js
+++ app/models/profile.js
@@ -1,7 +1,23 @@
 var db = require('../../config/db') ;
 //define profile model
 var Profile = db.model('Profile', {
-  name: {type: String, required: true}
+  name: {type: String, required: true},
+  email: {type: String, required: true},
+  location: {
+    street: {type: String, required: true},
+    country: {type: String, required: true},
+    city: {type: String, required: true},
+    lat: {type: Number, required: true},
+    lng: {type: Number, required: true}
+  },
+  products: [
+    {
+      name: {type: String, required: true},
+      cost: {type: Number, required: true},
+      sellBy: {type: String, required: true},
+      posted: {type: String, required: true}
+    }
+  ]
 }) ;
 
 module.exports = Profile ;
