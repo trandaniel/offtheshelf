8cb3303c27a5252950be3527d1ef0bb0d0065905
Daniel Tran <tran.daniel95@gmail.com>
2017-03-13 19:49:23 -0400

    Adding pages to business view

--- controllers/static.js
+++ controllers/static.js
@@ -1,12 +1,25 @@
 var express = require('express') ;
 var router  = express.Router() ;
 
+// routes for pages
 router.get('/', function(req, res) {
   res.sendfile('public/views/index.html') ;
 }) ;
 
-router.get('/signup', function(req, res) {
-  res.sendfile('public/views/business/signup.html') ;
+router.get('/signUp', function(req, res) {
+  res.sendfile('public/views/business/signUp.html') ;
+}) ;
+
+router.post('/confirm', function(req, res) {
+  res.sendfile('public/views/business/confirm.html') ;
+}) ;
+
+router.get('/editprofile', function(req, res) {
+  res.sendfile('public/views/business/editProfile.html')
+}) ;
+
+router.get('/addproduct', function(req, res) {
+  res.sendfile('public/views/business/addProduct.html')
 }) ;
 
 router.use(express.static(__dirname + '/../assets')) ;
