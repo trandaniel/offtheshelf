75efd329c1511c77aa7a8b81486a24c389233406
Daniel Tran <tran.daniel95@gmail.com>
2017-03-13 13:09:50 -0400

    Adding signup page and making it responsive on different screen sizes

--- controllers/static.js
+++ controllers/static.js
@@ -5,6 +5,10 @@ router.get('/', function(req, res) {
   res.sendfile('public/views/index.html') ;
 }) ;
 
+router.get('/signup', function(req, res) {
+  res.sendfile('public/views/business/signup.html') ;
+}) ;
+
 router.use(express.static(__dirname + '/../assets')) ;
 
 module.exports = router ;
