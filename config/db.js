2300d380ebde22310fa55f2d7567f57421aa78cc
balinbanh <balinbanh@gmail.com>
2017-03-19 21:07:53 -0400

    Create login functionality

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose') ;
 
-var mongo_URI = process.env.mongo_URI || 'localhost' ;
+var mongo_URI = "mongodb://balinbanh:Chinaman1`@ds145379.mlab.com:45379/offtheshelf";//"mongodb://trandaniel:Il2ekf4d@ds145379.mlab.com:45379/offtheshelf";//process.env.mongo_URI || 'localhost' ;
 
 if (mongo_URI)
   mongoose.connect(mongo_URI, function(err) {
