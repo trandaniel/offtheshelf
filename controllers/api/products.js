c733eab5112402e8eee3933eac79999c94109273
balinbanh <balinbanh@gmail.com>
2017-03-28 15:16:46 -0400

    Resolves #10

--- controllers/api/products.js
+++ controllers/api/products.js
@@ -30,23 +30,19 @@ router.post('/', function(req, res, nxt) {
     if(err) {
       return nxt(err) ;
     }
-    console.log("adding");
     var currentProductList = [];
     Profile.findOne({email: req.body.email}, function(err, profile) {
       if(err) {
         return nxt(err) ;
       }
-      console.log("searching");
-      console.log(msg._id);
       if (profile.prodIds != null || profile.prodIds != undefined) {
         currentProductList = profile.prodIds;
       }
       currentProductList.push((msg._id).toString());
-      console.log("list:" , currentProductList);
     }) ;
     Profile.findOneAndUpdate({email: req.body.email}, {"$push": {"prodIds": msg._id}}, {new:true}, function(err, profile){
      console.log(err);
-     res.json(201, profile);
+     res.redirect(201, '../../addProduct');
     });
   });
 
