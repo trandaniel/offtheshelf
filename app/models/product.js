8ee0cb588a5ecf0fe60a3b79c8d94a32c57b55f9
Daniel Tran <tran.daniel95@gmail.com>
2017-04-03 18:23:32 -0400

    search results now display address

--- app/models/product.js
+++ app/models/product.js
@@ -1,14 +1,27 @@
 // import db
-var db = require('../../config/db') ;
-var mongoose = require('mongoose') ;
+var db        = require('../../config/db') ;
+var mongoose  = require('mongoose') ;
 
 // define product model
+var profileAddress = new mongoose.Schema({
+  profileId:    {type: String, required: true},
+  streetnumber: {type: String, required: true},
+  street:       {type: String, required: true},
+  country:      {type: String, required: true},
+  city:         {type: String, required: true},
+  lat:          {type: String, required: true},
+  lng:          {type: String, required: true}
+}, {
+  _id: false
+});
+
 var productSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  cost: {type: Number, required: true},
-  stock: {type: Number, required: true},
-  sellBy: {type: String, required: true},
-  posted: {type: String, required: true}
+  name:     {type: String, required: true},
+  cost:     {type: Number, required: true},
+  stock:    {type: Number, required: true},
+  sellBy:   {type: String, required: true},
+  posted:   {type: String, required: true},
+  profile:  profileAddress
 }, {
   versionKey: false
 }) ;
